ec27b0988297c4f7aa54e68bac8c7033cebc7c6e
unknown <andy39866821@gmail.com>
2022-06-13 20:35:15 +0800

    update

--- src/arbiter.h
+++ src/arbiter.h
@@ -3,7 +3,8 @@
 
 #include "define.h"
 
-typedef struct{
+typedef struct
+{
     // Combinational wires
     int RR_counter_next[PORT_COUNT];
     int flit_counter_next[PORT_COUNT];
@@ -14,20 +15,16 @@ class arbiter
 private:
     int RR_counter[PORT_COUNT] = {0};
     int flit_counter[PORT_COUNT] = {0};
-public:
-    arbiter(/* args */);
 
+public:
     void update_combinational(
         bool output_VC_full[PORT_COUNT],
         bool request[PORT_COUNT][PORT_COUNT],
         bool grant[PORT_COUNT][PORT_COUNT],
-        arbiter_internal_wire* output_wire
-    );
+        arbiter_internal_wire *output_wire);
 
     void update_sequential(
-        arbiter_internal_wire input_wire
-    );
+        arbiter_internal_wire input_wire);
 };
-
 
 #endif
